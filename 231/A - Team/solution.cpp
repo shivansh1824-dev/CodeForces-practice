1e41e34688cecbdbfa3d3e1647bc07030650adf8
shivansh1824-dev <shivanshrai282@gmail.com>
2026-09-03 03:45:19 +0530

    Add Team [A] from Codeforces

---
+++ 231/A - Team/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    int count=0;
+    cin >> t;
+    while(t--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        if(a+b+c >= 2){
+            count++;
+        }
+    
+}
+cout << count << endl;
+return 0;
+}
